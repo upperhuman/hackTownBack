ddf2c9eaa545c51894847c8029b0b807906fa85e
flesh <fleshgolub@gmail.com>
2024-11-13 14:04:12 +0200

    refactor structure of project

---
+++ HackTownBack/Program.cs
@@ -0,0 +1,34 @@
+using HackTownBack.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<HackTownDbContext>(option => option.UseNpgsql(builder.Configuration.GetConnectionString("DBConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<HackTownDbContext>();
+    db.Database.Migrate();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
