64742eed306e40d0c941a842018490791fd71117
flesh <fleshgolub@gmail.com>
2024-12-26 20:38:49 +0200

    some code refactor

--- HackTownBack/Program.cs
+++ HackTownBack/Program.cs
@@ -32,11 +32,11 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-string dbHost = Environment.GetEnvironmentVariable("DATABASE_HOST") ?? "localhost";
+string dbHost = Environment.GetEnvironmentVariable("DATABASE_HOST") ?? "hacktowndb.cbu20qgcsbot.eu-north-1.rds.amazonaws.com";
 string dbPort = Environment.GetEnvironmentVariable("DATABASE_PORT") ?? "5432";
-string dbName = Environment.GetEnvironmentVariable("DATABASE_NAME") ?? "HackTownDB";
+string dbName = Environment.GetEnvironmentVariable("DATABASE_NAME") ?? "hacktowndb";
 string dbUser = Environment.GetEnvironmentVariable("DATABASE_USER") ?? "postgres";
-string dbPassword = Environment.GetEnvironmentVariable("DATABASE_PASSWORD") ?? "54321";
+string dbPassword = Environment.GetEnvironmentVariable("DATABASE_PASSWORD") ?? "585662vcxz";
 
 string connectionString = $"Host={dbHost};Port={dbPort};Database={dbName};Username={dbUser};Password={dbPassword}";
 
