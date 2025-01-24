1ce918400ded85260d7ce69ff2f27a1bb02f1fc1
flesh <fleshgolub@gmail.com>
2025-01-24 14:45:45 +0200

    add migrations

--- HackTownBack/Migrations/20250124123238_updUserRequset.Designer.cs
+++ HackTownBack/Migrations/20250124123238_updUserRequset.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HackTownBack.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace HackTownBack.Migrations
 {
     [DbContext(typeof(HackTownDbContext))]
-    partial class HackTownDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250124123238_updUserRequset")]
+    partial class updUserRequset
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HackTownBack/Migrations/20250124123238_updUserRequset.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HackTownBack.Migrations
+{
+    /// <inheritdoc />
+    public partial class updUserRequset : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PeopleCount",
+                table: "UserRequests",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CostTier",
+                table: "UserRequests",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Text",
+                table: "UserRequests",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Text",
+                table: "UserRequests");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PeopleCount",
+                table: "UserRequests",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CostTier",
+                table: "UserRequests",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+    }
+}
